7b9a774c609378b9d1c6e19ff75a8a3402c551db
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-12-07 17:03:35 +0530

    updating file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ import A1 from "../assets/img/a1.png";
 import Icon1 from "../assets/img/icon1.png";
 import Icon2 from "../assets/img/icon2.png";
 import Icon3 from "../assets/img/icon3.png";
+import { Rating } from "@mui/material";
 
 export default function HomePage() {
   return (
@@ -62,7 +63,6 @@ export default function HomePage() {
             <h1 className="text-sm ml-3">Shahi snan dates of MahaKumbh 2025</h1>
             <div className="h-12 border-l-2 border-gray-400 px-2"></div>
 
-
             <img src={Icon2} alt="Icon1" className="w-12 h-12" />
             <h1 className="text-sm ml-3">
               Top Attractions of Prayagraj and Mahakumbh
@@ -76,39 +76,162 @@ export default function HomePage() {
       </div>
 
       {/* why choose us */}
-        <div className="w-11/12 mx-auto my-8
+      <div
+        className="w-11/12 mx-auto my-8
         bg-[#FF9933] rounded-2xl shadow-lg p-4
-        ">
-            <h1 className="text-3xl font-bold text-center text-black">
-            Why Choose Us?
+        "
+      >
+        <h1 className="text-3xl font-bold text-center text-black">
+          Why Choose Us?
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              Local Experts
             </h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
-            <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-                <h1 className="text-xl font-bold text-[#F49330]">Expert Guidance</h1>
-                <p className="mt-2">
-                Our team of experienced professionals is dedicated to providing
-                you with authentic insights, guidance, and support throughout your
-                spiritual journey.
-                </p>
-            </div>
-            <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-                <h1 className="text-xl font-bold text-[#F49330]">Customized Tours</h1>
-                <p className="mt-2">
-                We offer a range of customizable tour packages to suit your
-                preferences, budget, and requirements, ensuring a memorable and
-                hassle-free experience.
-                </p>
-            </div>
-            <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-                <h1 className="text-xl font-bold text-[#F49330]">24/7 Support</h1>
-                <p className="mt-2">
-                Our dedicated customer support team is available round the clock
-                to address your queries, provide assistance, and ensure a
-                seamless experience.
-                </p>
-            </div>
-            </div>
+            <p className="mt-2">
+              We are born and raised here! Our deep knowledge of the city gives
+              you an authentic and personalized travel experience.
+            </p>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              Unique Experiences
+            </h1>
+            <p className="mt-2">
+              Skip the generic tour routes! We take you to hidden gems and
+              off-the-beaten-path locations for a memorable journey.
+            </p>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              Delicious Street Food
+            </h1>
+            <p className="mt-2">
+              Taste the most authentic street foods! From spicy chaat to
+              mouth-watering kebabs, we guide you on a food journey like no
+              other.
+            </p>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              New Yet Committed{" "}
+            </h1>
+            <p className="mt-2">
+              We are new, but our passion drives us to go the extra mile,
+              ensuring every moment of your trip is memorable and joyful.
+            </p>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              Personalized & Flexible Tours{" "}
+            </h1>
+            <p className="mt-2">
+              No rigid schedules! We tailor our tours to fit your preferences,
+              offering you the flexibility to enjoy at your own pace.
+            </p>
+          </div>{" "}
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              Transparent Pricing{" "}
+            </h1>
+            <p className="mt-2">
+              No hidden charges! We offer fair pricing and great value, making
+              sure you get the best experience for your money.
+            </p>
+          </div>
         </div>
+
+        <div className="text-center text-2xl font-semibold mt-4">
+          *We promise a memorable journey that goes beyond the usual travel
+          experience. Let us make your trip unforgettable!
+        </div>
+      </div>
+
+      {/* Trusted by 500+ Devotees  */}
+      <div
+        className="w-11/12 mx-auto my-8
+        bg-[#FF9933] rounded-2xl shadow-lg p-4
+        "
+      >
+        <h1 className="text-3xl font-bold text-center text-black">
+          Trusted by 500+ Devotees
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;The travel package was well-organized, and every detail was
+              taken care of. Highly recommended!&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Rahul Verma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;A truly divine experience. The accommodations and travel
+              arrangements were excellent!&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Priya Sharma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;Seamless booking process and great customer support. Made my
+              trip stress-free!&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Ankit Joshi
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;The tour was fantastic, but the transport could have been a
+              bit more comfortable.&quot;
+            </p>
+            <Rating name="read-only" value={3} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Neha Kapoor
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;Best pilgrimage tour experience ever! Will definitely
+              recommend to my friends.&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Suresh Nair
+            </h1>
+          </div>
+
+          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2">
+              &quot;Good value for money. However, I would have preferred more meal options.&quot;
+            </p>
+            <Rating name="read-only" value={4} readOnly className="pt-4" />
+
+            <h1 className="text-xl font-bold text-[#F49330] text-center">
+              {" "}
+              ~Vishal Singh
+            </h1>
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
