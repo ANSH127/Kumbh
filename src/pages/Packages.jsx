0a29fe1353ae610e1e85725c17ae2a5fb8364b82
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-12-09 22:50:13 +0530

    updating file

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -23,7 +23,7 @@ export default function Packages() {
       <Navbar />
       <div className="flex justify-center items-center mt-[10%] md:mt-[8%]  w-full">
         {/* Main Container */}
-        <div className=" w-full md:w-[90%] md:rounded-3xl shadow-lg overflow-hidden">
+        <div className=" w-full md:w-[90%]  md:rounded-3xl shadow-lg overflow-hidden">
           {/* Top Section with Background Image and Overlay Text */}
           <div className="relative h-48 md:h-64">
             {/* Background Image (Replace with actual image) */}
@@ -69,7 +69,7 @@ export default function Packages() {
         </div>
       </div>
       <div>
-        <div className="container mx-auto grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 md:px-0 mt-10">
+        <div className="w-full md:w-[90%] container mx-auto grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 md:px-0 mt-10">
           {/* Card Template */}
 
 
@@ -78,7 +78,9 @@ export default function Packages() {
               key={item._id}
               className="flex flex-col relative overflow-hidden"
             >
-              <img src={builder.image(item.image).url()} alt="" className="rounded-t-2xl w-auto h-[140px] md:h-auto " />
+              <img src={builder.image(item.image).url()} alt="" className="rounded-t-2xl w-auto h-[140px] 
+              md:h-[200px] object-cover object-center shadow-lg transition duration-300 hover:scale-105
+               " />
               <div className="bg-orange-400 rounded-b-2xl pb-4">
                 <div className="bg-[#F4F2E9] p-1 text-center -mt-8 mx-4 rounded-2xl shadow-lg z-10 relative font-serif">
                   <h3 className="text-xs md:text-base font-semibold">
@@ -115,131 +117,6 @@ export default function Packages() {
               </div>
             </div>
           ))}
-
-          <div className="flex flex-col relative overflow-hidden">
-            <img
-              src={Hero3}
-              alt=""
-              className="rounded-t-2xl w-auto h-[140px] md:h-auto "
-            />
-            <div className="bg-orange-400 rounded-b-2xl pb-4">
-              <div className="bg-[#F4F2E9] p-1 text-center -mt-8 mx-4 rounded-2xl shadow-lg z-10 relative font-serif">
-                <h3 className="text-xs md:text-base font-semibold">
-                  Non-Shahi Snan Kumbh Mela
-                </h3>
-                <p className="text-xs md:text-sm text-gray-600">
-                  2N/3D Tour Package
-                </p>
-              </div>
-
-              <div className="text-center pb-3">
-                <p className=" text-xl md:text-3xl font-extrabold text-center h-5 text-black my-4">
-                  ₹31,999
-                </p>
-                <del className="text-sm md:text-lg font-semibold text-black text-center">
-                  ₹35,999
-                </del>
-              </div>
-              <div className="flex justify-evenly gap-2">
-                <button
-                  className="bg-white px-1 mx-2 text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/packages/1")}
-                >
-                  Details →
-                </button>
-
-                <button
-                  className="bg-white px-1 mx-2  text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/enquiry")}
-                >
-                  Enquire Now →
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative overflow-hidden">
-            <img
-              src={Hero3}
-              alt=""
-              className="rounded-t-2xl w-auto h-[140px] md:h-auto "
-            />
-            <div className="bg-orange-400 rounded-b-2xl pb-4">
-              <div className="bg-[#F4F2E9] p-1 text-center -mt-8 mx-4 rounded-2xl shadow-lg z-10 relative font-serif">
-                <h3 className="text-xs md:text-base font-semibold">
-                  Non-Shahi Snan Kumbh Mela
-                </h3>
-                <p className="text-xs md:text-sm text-gray-600">
-                  2N/3D Tour Package
-                </p>
-              </div>
-
-              <div className="text-center pb-3">
-                <p className=" text-xl md:text-3xl font-extrabold text-center h-5 text-black my-4">
-                  ₹31,999
-                </p>
-                <del className="text-sm md:text-lg font-semibold text-black text-center">
-                  ₹35,999
-                </del>
-              </div>
-              <div className="flex justify-evenly gap-2">
-                <button
-                  className="bg-white px-1 mx-2 text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/packages/1")}
-                >
-                  Details →
-                </button>
-
-                <button
-                  className="bg-white px-1 mx-2  text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/enquiry")}
-                >
-                  Enquire Now →
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col relative overflow-hidden">
-            <img
-              src={Hero3}
-              alt=""
-              className="rounded-t-2xl w-auto h-[140px] md:h-auto "
-            />
-            <div className="bg-orange-400 rounded-b-2xl pb-4">
-              <div className="bg-[#F4F2E9] p-1 text-center -mt-8 mx-4 rounded-2xl shadow-lg z-10 relative font-serif">
-                <h3 className="text-xs md:text-base font-semibold">
-                  Non-Shahi Snan Kumbh Mela
-                </h3>
-                <p className="text-xs md:text-sm text-gray-600">
-                  2N/3D Tour Package
-                </p>
-              </div>
-
-              <div className="text-center pb-3">
-                <p className=" text-xl md:text-3xl font-extrabold text-center h-5 text-black my-4">
-                  ₹31,999
-                </p>
-                <del className="text-sm md:text-lg font-semibold text-black text-center">
-                  ₹35,999
-                </del>
-              </div>
-              <div className="flex justify-evenly gap-2">
-                <button
-                  className="bg-white px-1 mx-2 text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/packages/1")}
-                >
-                  Details →
-                </button>
-
-                <button
-                  className="bg-white px-1 mx-2  text-xs py-1 md:text-base text-black font-semibold md:px-3 md:py-2 rounded-lg shadow hover:bg-gray-200 hover:shadow-md transition duration-300"
-                  onClick={() => navigate("/enquiry")}
-                >
-                  Enquire Now →
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
         <div className=" w-11/12   rounded-2xl mx-auto m-4  bg-[#FDAB57] flex flex-col justify-center  h-14">
           <h1 className="text-[5vw] md:text-[2.8vw] text-center font-bold my-6 md:my-[60px]">
