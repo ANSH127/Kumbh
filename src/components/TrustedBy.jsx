57625edc7dd0e43af2d751609ef28f6df8017946
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-12-11 00:46:09 +0530

    updating file

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -6,96 +6,86 @@ export default function TrustedBy() {
         <h1 className="text-3xl font-bold text-center text-black">
           Trusted by 500+ Devotees
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-24 gap-y-8 mt-4 p-10">
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;The travel package was well-organized, and every detail was
-              taken care of. Highly recommended!&quot;
+        <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4 mt-4 md:p-4">
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
             </p>
-            <Rating name="read-only" value={5} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
               ~Rahul Verma
             </h1>
           </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;A truly divine experience. The accommodations and travel
-              arrangements were excellent!&quot;
-            </p>
-            <Rating name="read-only" value={5} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Priya Sharma
-            </h1>
-          </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Seamless booking process and great customer support. Made my
-              trip stress-free!&quot;
-            </p>
-            <Rating name="read-only" value={5} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Ankit Joshi
-            </h1>
-          </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;The tour was fantastic, but the transport could have been a
-              bit more comfortable.&quot;
-            </p>
-            <Rating name="read-only" value={3} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Neha Kapoor
-            </h1>
-          </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Best pilgrimage tour experience ever! Will definitely
-              recommend to my friends.&quot;
-            </p>
-            <Rating name="read-only" value={5} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Suresh Nair
-            </h1>
-          </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Good value for money. However, I would have preferred more
-              meal options.&quot;
-            </p>
-            <Rating name="read-only" value={4} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Vishal Singh
-            </h1>
-          </div>
 
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Good value for money. However, I would have preferred more
-              meal options.&quot;
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
             </p>
-            <Rating name="read-only" value={4} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Vishal Singh
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
             </h1>
           </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Good value for money. However, I would have preferred more
-              meal options.&quot;
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
             </p>
-            <Rating name="read-only" value={4} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Vishal Singh
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
             </h1>
           </div>
-          <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg text-center">
-            <p className="mt-2">
-              &quot;Good value for money. However, I would have preferred more
-              meal options.&quot;
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
             </p>
-            <Rating name="read-only" value={4} readOnly className="pt-4" />
-            <h1 className="text-xl font-bold text-black text-center">
-              ~Vishal Singh
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
+            </h1>
+          </div>
+          <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg text-center">
+            <p className="mt-2  text-sm md:text-base
+          text-justify">
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illo praesentium eius dolores facere recusandae.&quot;
+            </p>
+            <Rating name="read-only" value={5} readOnly className=" pt-1 md:pt-4" />
+            <h1 className="text-sm md:text-xl font-bold text-black text-center">
+              ~Rahul Verma
             </h1>
           </div>
         </div>
--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,63 +1,83 @@
-
 export default function WhyChooseUs() {
   return (
     <div className="w-11/12 mx-auto my-8 bg-[#FF9933] rounded-2xl shadow-lg p-4">
       <h1 className="text-3xl font-bold text-center text-black">
         Why Choose Us?
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-24 gap-y-8 mt-4 p-10">
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
+      <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4 mt-4 md:p-4">
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
             Local Experts
           </h1>
-          <p className="mt-2">
+          <p className="mt-2 text-sm md:text-base">
             We are born and raised here! Our deep knowledge of the city gives
             you an authentic and personalized travel experience.
           </p>
         </div>
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
-            Unique Experiences
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
           </h1>
-          <p className="mt-2">
-            Skip the generic tour routes! We take you to hidden gems and
-            off-the-beaten-path locations for a memorable journey.
+          <p className="mt-2 text-sm md:text-base
+          text-justify
+          ">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
           </p>
         </div>
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
-            Delicious Street Food
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
           </h1>
-          <p className="mt-2">
-            Taste the most authentic street foods! From spicy chaat to
-            mouth-watering kebabs, we guide you on a food journey like no other.
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
           </p>
         </div>
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
-            New Yet Committed
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
           </h1>
-          <p className="mt-2">
-            We are new, but our passion drives us to go the extra mile, ensuring
-            every moment of your trip is memorable and joyful.
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
           </p>
         </div>
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
-            Personalized & Flexible Tours
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
           </h1>
-          <p className="mt-2">
-            No rigid schedules! We tailor our tours to fit your preferences,
-            offering you the flexibility to enjoy at your own pace.
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
           </p>
         </div>
-        <div className="bg-[#F4F2E9] p-4 rounded-lg shadow-lg">
-          <h1 className="text-xl font-bold text-black text-center">
-            Transparent Pricing
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
           </h1>
-          <p className="mt-2">
-            No hidden charges! We offer fair pricing and great value, making
-            sure you get the best experience for your money.
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
+          </p>
+        </div>
+
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
+          </h1>
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
+          </p>
+        </div>
+        <div className="bg-[#F4F2E9] p-3 h-fit md:p-4 rounded-lg shadow-lg">
+          <h1 className="text-sm md:text-xl font-bold text-black text-center">
+            Local Experts
+          </h1>
+          <p className="mt-2 text-sm md:text-base text-justify">
+            We are born and raised here! Our deep knowledge of the city gives
+            you an authentic and personalized travel experience.
           </p>
         </div>
       </div>
