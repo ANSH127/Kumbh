88237e7d901cc552e3f5800ee5ab05cabc9c509c
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-12-20 14:34:09 +0530

    refactor Footer component to use React Router Links for navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,184 +1,6 @@
 // import logo from "../assets/img/logo.png";
-// import footerImage from "../assets/img/footer.png";
-
-// const Footer = () => {
-//   return (
-//     <footer className="bg-[#003F50] pt-8 text-white">
-//       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-10 text-center sm:px-4 ">
-//         {/* About Us Section */}
-//         <div className="text-[4vw] md:text-[1.5vw] font-medium">
-//           <h1 className="text-[#ffcc00]">About Us</h1>
-//           {/* <img className="w-24 h-auto mx-auto mt-2" src={logo} alt="Logo"/> */}
-
-//           <p className="text-[3vw] sm:text-[2vw] md:text-[1.1vw] mt-2 leading-5 md:leading-6 text-gray-200">
-//             Welcome to www.prayagrajkumbhmela.com, your guide to experiencing the spiritual
-//             essence of Kumbh Mela. Dive into authentic insights, rituals, and
-//             services that celebrate India's cultural heritage.
-//           </p>
-//         </div>
-
-//         {/* Contact Us Section */}
-//         <div className="text-[4vw] md:text-[1.5vw] font-medium">
-//           <h1 className="text-[#ffcc00]">Contact Us</h1>
-//           <p className="text-[3.5vw] md:text-[1.2vw] leading-2 text-gray-200 ">
-//             Email:{" "}
-//             <a
-//               href="mailto:kumbhexperience@gmail.com"
-//               className="hover:underline"
-//             >
-//               kumbhexperienceindia@gmail.com
-//             </a>
-//             <br />
-//             Phone:{" "}
-//             <a href="tel:+918932951318" className="hover:underline">
-//               +91 8932951318
-//             </a>
-//           </p>
-//           <p className="text-[3.5vw] md:text-[1.2vw] mt-2 leading-2 text-gray-200">
-//             Address: Mutthiganj, Prayagraj, Uttar Pradesh
-//           </p>
-//           <div className="flex justify-center gap-6 mt-4 text-[6vw] md:text-[2vw]">
-//             <a
-//               href="https://www.instagram.com/prayagraj.kumbhmela/"
-//               className="hover:text-[#E1306C]"
-//               aria-label="Instagram"
-//             >
-//               <i className="fab fa-instagram"></i>
-//             </a>
-//             <a
-//               href="mailto:kumbhexperienceindia@gmail.com"
-//               className="hover:text-[#D44638]"
-//               aria-label="Gmail"
-//             >
-//               <i className="fas fa-envelope"></i>
-//             </a>
-//             <a
-//               href="https://wa.me/+918932951318"
-//               className="hover:text-[#25D366]"
-//               aria-label="WhatsApp"
-//             >
-//               <i className="fab fa-whatsapp"></i>
-//             </a>
-//             <a
-//               href="https://www.linkedin.com/in/prayagraj-kumbh-mela-176b03339/"
-//               className="hover:text-[#0077b5]"
-//               aria-label="LinkedIn"
-//             >
-//               <i className="fab fa-linkedin"></i>
-//             </a>
-//             <a
-//               href="https://www.facebook.com/profile.php?id=61569489230062"
-//               className="hover:text-[#1877F2]"
-//               aria-label="Facebook"
-//             >
-//               <i className="fab fa-facebook"></i>
-//             </a>
-//           </div>
-//         </div>
-
-//         {/* IMPORTANT Links Section */}
-       
-
-//         <div className="text-[3.5vw] md:text-[1.3vw] font-medium text-center">
-//           <h1 className="text-[#ffcc00] mb-2">Important Links</h1>
-//           <div className="flex flex-col space-y-2">
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/AboutUs"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               AboutUs
-//             </a>
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/PrivacyPolicy"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Privacy Policy
-//             </a>
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/RefundPolicy"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Refund Policy
-//             </a>
-
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/enquiry"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Tour Enquiry
-//             </a>
-
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/TermsAndConditions"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Terms And Conditions
-//             </a>
-
-            
-            
-//           </div>
-//         </div>
-
-//         {/* Blogs Links Section */}
-
-//         <div className="text-[3.5vw] md:text-[1.3vw] font-medium mx-auto w-fit">
-//           <h1 className="text-[#ffcc00] mb-2">Explore</h1>
-//           <div className="flex flex-col space-y-2 text-left">
-//             <a
-//               href="https://www.prayagrajkumbhmela.com/AboutUs"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               About Kumbh Mela
-//             </a>
-//             <a
-//               href="Blog1.html"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Rituals Of Kumbh
-//             </a>
-           
-//             <a
-//               href="Blog1.html"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Sadhu Rituals
-//             </a>
-//             <a
-//               href="Blog1.html"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Bathing Dates
-//             </a>
-//             <a
-//               href="Blog1.html"
-//               className="hover:text-[#ffcc00] hover:underline"
-//             >
-//               Famous Temples
-//             </a>
-//           </div>
-//         </div>
-
-//       </div>
-
-//       <div className="mt-6">
-//         <img className="w-full" src={footerImage}
-//          alt="Mahakumbh Tour Packages" />
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
-
-
-
-
-
-import logo from "../assets/img/logo.png";
 import footerImage from "../assets/img/footer.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -273,36 +95,36 @@ const Footer = () => {
             Important Links
           </h2>
           <nav className="flex flex-col space-y-2">
-            <a
-              href="https://www.prayagrajkumbhmela.com/AboutUs"
+            <Link
+              to="/AboutUs"
               className="hover:text-[#ffcc00] hover:underline"
             >
               About Us
-            </a>
-            <a
-              href="https://www.prayagrajkumbhmela.com/PrivacyPolicy"
+            </Link>
+            <Link
+              to="/PrivacyPolicy"
               className="hover:text-[#ffcc00] hover:underline"
             >
               Privacy Policy
-            </a>
-            <a
-              href="https://www.prayagrajkumbhmela.com/RefundPolicy"
+            </Link>
+            <Link
+              to="/RefundPolicy"
               className="hover:text-[#ffcc00] hover:underline"
             >
               Refund Policy
-            </a>
-            <a
-              href="https://www.prayagrajkumbhmela.com/enquiry"
+            </Link>
+            <Link
+              to="/enquiry"
               className="hover:text-[#ffcc00] hover:underline"
             >
               Tour Enquiry
-            </a>
-            <a
-              href="https://www.prayagrajkumbhmela.com/TermsAndConditions"
+            </Link>
+            <Link
+              to="/TermsAndConditions"
               className="hover:text-[#ffcc00] hover:underline"
             >
               Terms and Conditions
-            </a>
+            </Link>
           </nav>
         </section>
 
@@ -315,12 +137,12 @@ const Footer = () => {
             Explore
           </h2>
           <nav className="flex flex-col space-y-2">
-            <a
-              href="https://www.prayagrajkumbhmela.com/AboutUs"
+            <Link
+              to="/AboutUs"
               className="hover:text-[#ffcc00] hover:underline"
             >
               About Kumbh Mela
-            </a>
+            </Link>
             <a href="Blog1.html" className="hover:text-[#ffcc00] hover:underline">
               Rituals of Kumbh
             </a>
